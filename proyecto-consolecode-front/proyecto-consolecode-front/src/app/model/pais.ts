2c39efa3184aa0c47b8e0c277cf2088b78daef8b
CristianPSab <crpunti.sab@hotmail.com>
2021-10-16 12:57:54 +0200

    frontend

--- proyecto-consolecode-front/proyecto-consolecode-front/src/app/model/pais.ts
+++
@@ -1,5 +0,0 @@
-export class Pais {
-  idPais!: number;
-  nombre!: string;
-
-}
--- proyecto-consolecode-front/proyecto-consolecode-front/src/app/model/persona.ts
+++
@@ -1,9 +0,0 @@
-import { Pais } from "./pais";
-
-export class Persona {
-    idPersona!: number;
-    nombres!: string;
-    apellidos!: string;
-    edad!: number;
-    pais!: Pais;
-}
